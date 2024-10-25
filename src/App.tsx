9a2f61b94e8fe97877b6f859f9104834dd002de2
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-10-24 23:33:14 -0300

    FEAT: Iniciando o esqueleto em HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 export default function App() {
   return (
-    <main>
+    <main className="grid-rows-2">
       <h1 className="text-2xl font-bold text-center">Translated.io</h1>
       <div>
-        <section></section>
-        <section></section>
+        <section>
+          <header>
+            <button>Português</button>
+          </header>
+          <textarea name="toTranslate" id="toTranslate"></textarea>
+          <footer>
+            <button>Ouvir</button>
+            <button>Copiar</button>
+            <button>Translate</button>
+          </footer>
+        </section>
+        <section>
+          <header>
+            <button>English</button>
+            <button>Inverter</button>
+          </header>
+          <textarea name="toTranslate" id="toTranslate" readOnly></textarea>
+          <footer>
+            <button>Ouvir</button>
+            <button>Copiar</button>
+          </footer>
+        </section>
       </div>
     </main>
   );
