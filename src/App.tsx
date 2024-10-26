c23f29fdb043949bb549c3ed0856907db8fe366f
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-10-26 00:11:18 -0300

    FEAT: Inciando a estilização e aprendendo como utilizar o Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 export default function App() {
   return (
-    <main className="grid-rows-2">
-      <h1 className="text-2xl font-bold text-center">Translated.io</h1>
-      <div>
-        <section>
-          <header>
-            <button>Português</button>
-          </header>
-          <textarea name="toTranslate" id="toTranslate"></textarea>
-          <footer>
-            <button>Ouvir</button>
-            <button>Copiar</button>
-            <button>Translate</button>
-          </footer>
-        </section>
-        <section>
-          <header>
-            <button>English</button>
-            <button>Inverter</button>
-          </header>
-          <textarea name="toTranslate" id="toTranslate" readOnly></textarea>
-          <footer>
-            <button>Ouvir</button>
-            <button>Copiar</button>
-          </footer>
-        </section>
-      </div>
+    <main className="flex items-center justify-center flex-wrap gap-8">
+      <h1 className="text-2xl font-bold text-center basis-full mb-8">
+        Translated.io
+      </h1>
+      <section className="bg-slate-700 w-96 h-56">
+        <header>
+          <button>Português</button>
+        </header>
+        <textarea name="toTranslate" id="toTranslate"></textarea>
+        <footer>
+          <button>Ouvir</button>
+          <button>Copiar</button>
+          <button>Translate</button>
+        </footer>
+      </section>
+      <section className="bg-slate-700 bg-slate-700 w-96 h-56">
+        <header>
+          <button>English</button>
+          <button>Inverter</button>
+        </header>
+        <textarea name="toTranslate" id="toTranslate" readOnly></textarea>
+        <footer>
+          <button>Ouvir</button>
+          <button>Copiar</button>
+        </footer>
+      </section>
     </main>
   );
 }
