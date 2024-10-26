9ccfc868df364db23d89ff8dc9325b506d648790
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-10-26 20:54:06 -0300

    FEAT: Colocando o background principal

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        backgroundApp:
+          "url('./src/assets/bryan-goff-f7YQo-eYHdM-unsplash.jpg')",
+        // Image by: https://unsplash.com/pt-br/fotografias/aglomerado-de-ilustracao-de-estrela-f7YQo-eYHdM
+      },
       width: {
         38: "38%",
         128: "32rem",
